b02ef947e11e3c115e27116a0ba5e75e2db95871
fengweizhou <shiwoow@gmail.com>
2026-07-30 00:01:34 +0800

    Initial Commit

---
+++ TheHiddenFeature/TheHiddenFeatureApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheHiddenFeatureApp.swift
+//  TheHiddenFeature
+//
+//  Created by FengWeizhou on 2026/7/30.
+//
+
+import SwiftUI
+
+@main
+struct TheHiddenFeatureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
